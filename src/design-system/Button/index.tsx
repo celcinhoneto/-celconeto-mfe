d8833996a64cb94a8d3abdc88d593ac2ab742c66
Celço Neto <celcoroxa@gmail.com>
2023-02-21 17:52:11 -0300

    second

--- src/design-system/Button/index.tsx
+++ src/design-system/Button/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { ReactElement } from 'react';
+import styled from 'styled-components';
 
-export const Button = (): ReactElement => (
-  <button onClick={() => null}>Button</button>
+interface IButtonProps {
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  children: string;
+}
+
+const StyledButton = styled.button`
+  width: 10rem;
+  padding: 2rem;
+  background-color: cadetblue;
+`;
+
+export const Button = ({ onClick, children }: IButtonProps): ReactElement => (
+  <StyledButton onClick={onClick}>{children}</StyledButton>
 );
